9be6cc35aab2c4af8973d3f5f4f5d778895cc288
Daniel Polo <od.polo@gmail.com>
2018-10-26 14:58:37 -0500

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :exchanges, only: [:index, :show, :create, :update] do
+    get '/sort', action: :sort, controller: 'exchanges'
+    resources :people, only:[:create]
+  end
+  resources :people, only:[:show] do
+    resources :gifts, only:[:create]
+  end
+  root 'exchanges#index'
 end
+
